ea0254a6c9793042e232f5c39a95875a8613cc68
Harshada1234567 <40234330+Harshada1234567@users.noreply.github.com>
2018-08-06 15:58:43 +0530

    Add files via upload

---
+++ ThealthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tikox.thealth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThealthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
